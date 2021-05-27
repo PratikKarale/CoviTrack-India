284ae5aa6fddb1eb46872dc6d492a3f2b66b7217
Pratik <pratikkarale21gm@gmail.com>
2021-05-28 01:29:33 +0530

    performance improvemend

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const https = require('https');
+const { JSDOM } = require( "jsdom" );
+const { window } = new JSDOM( "" );
+const $ = require( "jquery" )( window );
+const ejsLint = require('ejs-lint');
 const { json } = require('body-parser');
 const { on } = require('events');
 const { stringify } = require('querystring');
 const { response } = require('express');
 const { exit } = require('process');
+const { stat } = require('fs');
+const { ADDRCONFIG } = require('dns');
+const { globalAgent } = require('http');
 
 const app = express();
 
@@ -13,16 +20,46 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
+var myObj = {"key": 12, "key2" : 34, "key3" : 144};
 
 var lastMonthData = [];
 var lastMonthDates = [];
 
+//Storing main info in separate array to put it into the table 
+var stateNames = [  "Andaman and Nicobar Islands",  "Andhra Pradesh",  "Arunachal Pradesh",  "Assam",  "Bihar",  "Chandigarh",  "Chhattisgarh",  "Delhi",  "Dadra and Nagar Haveli",  "Goa",  "Gujarat",  "Himachal Pradesh",  "Haryana",  "Jharkhand",  "Jammu and Kashmir",  "Karnataka",  "Kerala",  "Ladakh",  "Lakshadweep",  "Maharashtra",  "Meghalaya",  "Manipur",  "Madhya Pradesh",  "Mizoram",  "Nagaland",  "Odisha",  "Punjab",  "Puducherry",  "Rajasthan",  "Sikkim",  "Telangana",  "Tamil Nadu",  "Tripura",  "TT",  "Uttar Pradesh",  "Uttarakhand",  "West Bengal"];
+const url = "https://api.covid19india.org/v4/min/data.min.json";
+var stateConfirmed = [];
+var stateActive = [];
+var stateRecovered = [];
+var stateDeceased = [];
+var stateTested = [];
+var stateVaccinated = [];
+
+https.get(url, function(response){
+    var chunks = [];
+    response.on("data", function(data){
+        chunks.push(data);
+    }).on("end", function(){
+        var data = JSON.parse(Buffer.concat(chunks));
+        var i = 0; 
+        $.each(data, function(key, val){
+            stateConfirmed.push(Number(val.total.confirmed).toLocaleString());
+            stateRecovered.push(Number(val.total.recovered).toLocaleString());
+            stateDeceased.push(Number(val.total.deceased).toLocaleString());
+            stateTested.push(Number(val.total.tested).toLocaleString());
+            stateVaccinated.push(Number(val.total.vaccinated).toLocaleString());
+            stateActive.push((Number(val.total.confirmed) - (Number(val.total.recovered) + Number(val.total.deceased))).toLocaleString());
+            i++;
+        })
+    })
+})
+
 
 app.get("/", function(req, res){
-
     // sending country data on home page
     // url for country data
     const url = "https://api.covid19india.org/data.json";
+    // const url = "https://api.covid19india.org/v4/min/data.min.json";
     var chunks = [];
 
     https.get(url, function(response){
@@ -36,25 +73,31 @@ app.get("/", function(req, res){
             var totalConfirmed = latestData.totalconfirmed;
             var totalRecovered = latestData.totalrecovered;
             var totalDeaths = latestData.totaldeceased;
-            // console.log(latestData);
-            
-            // last 7 days data
-            var tempData  = data.cases_time_series;
-
-            lastMonthData = [];
-            lastMonthDates = [];
-            
-            for(let i = 30; i>0; i--){
-                lastMonthData.push(tempData[data.cases_time_series.length-i].dailyconfirmed);
-                lastMonthDates.push(tempData[data.cases_time_series.length-i].date);
-            }
-
+            //vaccination data            
             var vaccinationData = data.tested[data.tested.length-1];
             var firstDose = vaccinationData.firstdoseadministered;
             var fullyVaccinated = vaccinationData.seconddoseadministered;
             var percentage = ((fullyVaccinated/1380004385)*100).toFixed(2);
 
-            res.render("index", {dailyConfirmedEJS: dailyConfirmed, totalConfirmedEJS: totalConfirmed, totalRecoveredEJS: totalRecovered, totalDeathsEJS: totalDeaths, monthData: lastMonthData, monthDates: lastMonthDates, firstDoseEJS: firstDose, fullyVaccinatedEJS: fullyVaccinated, percentageFullyVaccinatedEJS: percentage});
+            dailyConfirmed = Number(dailyConfirmed).toLocaleString();
+            totalConfirmed = Number(totalConfirmed).toLocaleString();
+            totalRecovered = Number(totalRecovered).toLocaleString();
+            totalDeaths = Number(totalDeaths).toLocaleString();
+            firstDose = Number(firstDose).toLocaleString();
+            fullyVaccinated = Number(fullyVaccinated).toLocaleString();
+            
+            var tempData  = data.cases_time_series;
+            //setting last month data == null (if user searches multiple 
+            // times then last month data will be added again and again and this will cause a weird graph)
+            lastMonthData = [];
+            lastMonthDates = [];
+            //looping through the data nd getting last 30 day data
+            for(let i = 7; i>0; i--){
+                lastMonthData.push(tempData[data.cases_time_series.length-i].dailyconfirmed);
+                lastMonthDates.push(tempData[data.cases_time_series.length-i].date);
+            }
+
+            res.render("index", {dailyConfirmedEJS: dailyConfirmed, totalConfirmedEJS: totalConfirmed, totalRecoveredEJS: totalRecovered, totalDeathsEJS: totalDeaths, monthData: lastMonthData, monthDates: lastMonthDates, firstDoseEJS: firstDose, fullyVaccinatedEJS: fullyVaccinated, percentageFullyVaccinatedEJS: percentage, stateConfirmedEJS: stateConfirmed, stateActiveEJS: stateActive, stateRecoveredEJS: stateRecovered, stateTestedEJS:stateTested, stateVaccinatedEJS: stateVaccinated, stateDeceasedEJS: stateDeceased, stateNamesEJS:stateNames});
 
         })
 
@@ -63,52 +106,61 @@ app.get("/", function(req, res){
 
 
 app.post("/", function(req, res){
+    console.log(req.body);
+    var state = req.body.stateSelect;
+    var district = req.body.districtSelect;
 
-    var district = (req.body.districtInput);
-    var stateOfDistrictInput = 'null';
-    var url_states = "https://raw.githubusercontent.com/thatisuday/indian-cities-database/master/cities.json";
+    if(!district || !state){                 //if the input is not entered properly
+        res.redirect("/failure");
+    }else{
 
-    https.get(url_states, function(response){
-        var chunks = [];
-        response.on("data", function(data){
-            chunks.push(data);
-        }).on("end", function(){
-            var data = JSON.parse(Buffer.concat(chunks));
-            for(var i = 0; i<1481 ; i++){
-                if(data[i].city == district){
-                    stateOfDistrictInput = data[i].state;
-                    console.log(stateOfDistrictInput);
-                }
-            }
-        })
-    })
 
-    //url for district wise data
-    const url = "https://api.covid19india.org/state_district_wise.json";
-    https.get(url, function(response){
-        var chunks = [];
-        response.on("data", function(data){
-            chunks.push(data);
-        }).on("end", function(){
-            var data = JSON.parse(Buffer.concat(chunks));
-            if(stateOfDistrictInput === 'null'){
-                res.render('result', {activeEJS: "-", confirmedEJS: "-", deathsEJS: "-", districtEJS: "err", monthData: lastMonthData, monthDates: lastMonthDates})
-            }else{
-                console.log(data[stateOfDistrictInput].districtData[district]);
-                var active = data[stateOfDistrictInput].districtData[district].active;
-                var confirmed = data[stateOfDistrictInput].districtData[district].confirmed;
-                var deceased = data[stateOfDistrictInput].districtData[district].deceased;
+        //url for district wise data
+        const url = "https://api.covid19india.org/v4/min/data.min.json";
+        https.get(url, function(response){
+            var chunks = [];
+            response.on("data", function(data){
+                chunks.push(data);
+            }).on("end", function(){
+                var data = JSON.parse(Buffer.concat(chunks));
+                // console.log(data[state].districts[district]);
+                var confirmed = data[state].districts[district].total.confirmed;
+                var recovered = data[state].districts[district].total.recovered;
+                var vaccinated = data[state].districts[district].total.vaccinated;
+                var deceased = data[state].districts[district].total.deceased;
+                var active = Number(confirmed) - Number(recovered+deceased);
+                var population = data[state].districts[district].meta.population;
+                
+                confirmed = Number(confirmed).toLocaleString();
+                recovered = Number(recovered).toLocaleString();
+                vaccinated = Number(vaccinated).toLocaleString();
+                deceased = Number(deceased).toLocaleString();
+                active = (active).toLocaleString();
+                population = Number(population).toLocaleString();
 
-                res.render('result', {activeEJS: active, confirmedEJS: confirmed, districtEJS: district, deathsEJS: deceased, monthData: lastMonthData, monthDates: lastMonthDates})
-            }
-        })
-        
-    });
+                console.log(active);
+                console.log(deceased);
+
+                res.render('result', {activeEJS: active, confirmedEJS: confirmed, districtEJS: district, deathsEJS: deceased, monthData: lastMonthData, monthDates: lastMonthDates, stateEJS: state, populationEJS:population, stateConfirmedEJS: stateConfirmed, stateActiveEJS: stateActive, stateRecoveredEJS: stateRecovered, stateTestedEJS:stateTested, stateVaccinatedEJS: stateVaccinated, stateDeceasedEJS: stateDeceased, stateNamesEJS:stateNames})
+            })
+            
+        });
+
+
+
+    }
+
     
 })
 
 
+app.get("/failure", function(req, res){
+    res.render('failure');
+})
 
+app.post("/failure", function(req, res){
+    res.redirect("/");
+})
 
 
 app.listen(process.env.PORT||3000, function(){
--- public/chart/myChart.js
+++ public/chart/myChart.js
@@ -4,50 +4,57 @@ let b = document.querySelectorAll(".monthDates");
 let monthDataArray = [];
 let monthDatesArray = [];
 
-for(var i = 0; i<a.length; i++){
+for (var i = 0; i < a.length; i++) {
   monthDataArray.push(a[i].innerText);
 }
 
-for(var i = 0; i<b.length; i++){
+for (var i = 0; i < b.length; i++) {
   monthDatesArray.push(b[i].innerText);
 }
 
 console.log(a);
 
 var ctx = document.getElementById("myChart");
-      var myChart = new Chart(ctx, {
-        // type: 'line',
-        data: {
-          labels: monthDatesArray,
-          datasets: [{
-            type: 'line',
-            data: monthDataArray,
-            lineTension: 0.2,
-            backgroundColor: 'transparent',
-            borderColor: 'red',
-            borderWidth: 2,
-            pointBackgroundColor: 'red',
-            pointRadius: 1,
-          }]
-        },
+var myChart = new Chart(ctx, {
+  type: "line",
+  data: {
+    labels: monthDatesArray,
+    datasets: [
+      {
+        type: "line",
+        data: monthDataArray,
+        lineTension: 0.2,
+        backgroundColor: "transparent",
+        borderColor: "#007bff",
+        borderWidth: 2,
+        pointBackgroundColor: "#007bff",
+        pointRadius: 1,
+        backgroundColor: "#007bff20",
+      },
+    ],
+  },
 
-        options: {
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero: false,
-              }
-            }],
-
-            xAxes: [{
-              gridLines:{
-                display:false
-              }
-            }]
+  options: {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: false,
           },
+        },
+      ],
 
-          legend: {
+      xAxes: [
+        {
+          gridLines: {
             display: false,
-          }
-        }
-      });
+          },
+        },
+      ],
+    },
+
+    legend: {
+      display: false,
+    },
+  },
+});
